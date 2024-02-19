745ecc7e2a713ef84e69931f8b7e1cb4b9e6ae47
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-02-19 14:44:21 +0100

    add seed utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+"""
+This file contains the parameters that are used in this project
+"""
+
+# Random seed 
+seed = 123454321
+
+#Parameter for black-box model as neuralnetwork
+
+#Parameter for generator
+
