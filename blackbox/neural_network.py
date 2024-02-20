fd282f15eaec7c3102f6378d5990d95af7544db5
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-02-20 13:02:16 +0100

    add finale neural_network.py

--- blackbox/neural_network.py
+++ blackbox/neural_network.py
@@ -14,24 +14,51 @@ import numpy as np
 from torchinfo import summary
 from sklearn.metrics import f1_score, precision_score, recall_score
 
+"""
+Output of this file is the pretrained blackbox model as neural network on COMPAS dataset. User can run this file with syntax from argparse to test our file:
+
+    python neuralnetwork.py --name "yourname" --hidden_size "your_hidden_size" --hidden_layers "your_hidden_layers" --lr "your_lr" --epoch "your_epoch" --wd "your_wd"
+    
+Then the output should be yourname.pth. Default of the arguments:
+
+--name: "neuralnetwork" and our output files is neural_network.pth
+--hidden_size: 16
+--hidden_layers: 10
+--lr: 0.01
+--epoch: 2000
+--wd: 1e-5
+"""
+
 #fix parameter and constante
 SEED = utils.seed
 torch.manual_seed(SEED)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("device:", device)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed(SEED)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
-TEST_SIZE = utils.test_size
 INPUT_SIZE = utils.input_size 
-HIDDEN_SIZE = utils.hidden_size
-HIDDEN_LAYERS = utils.hidden_layers 
+TEST_SIZE = utils.test_size
 OUTPUT_SIZE = utils.output_size 
-LR = utils.lr_blackbox
-EPOCH = utils.epoch_blackbox
-WD = utils.wd_blackbox
+
+# add argument
+parse = argparse.ArgumentParser()
+parse.add_argument("--name", type=str, default=utils.name_blackbox_nn,
+                   help="name of the pretrained blackbox model name.pth, default is 'neural_network'")
+parse.add_argument("--hidden_size", type=int, default=utils.hidden_size,
+                   help="number of hidden units in one hidden layer in model, default is 16")
+parse.add_argument("--hidden_layers", type=int, default=utils.hidden_layers,
+                   help="number of hidden layers in model, default is 10")
+parse.add_argument("--lr", type=float, default=utils.lr_blackbox,
+                   help="learning rate, default is 0.01")
+parse.add_argument("--epoch", type=int, default=utils.epoch_blackbox,
+                   help="epoch, default is 2000")
+parse.add_argument("--wd", type=float, default=utils.wd_blackbox,
+                   help="weight decay, default is 1e-5")
+
+# read the argument
+args = parse.parse_args()
+NAME = args.name
+HIDDEN_SIZE = args.hidden_size
+HIDDEN_LAYERS = args.hidden_layers 
+LR = args.lr
+EPOCH = args.epoch
+WD = args.wd
 
 #load data
 name_X = utils.name_preprocessed_data_X
@@ -90,8 +117,6 @@ class BlackBoxModel(nn.Module):
         for hid_layer in self.hidden_layers:
             x = hid_layer(x)
             x = self.relu(x)
-        #x = self.hidden_layers(x)
-        x = self.relu(x)
         x = self.output_layer(x)
         return x
 
@@ -157,16 +182,10 @@ for epoch in range(EPOCH):
         print('loss test = {}, accuracy test = {} , f1 test {}'.format(loss_test, accuracy_test, f1_test))
         print()
 
-y_out = model(X_test).ravel()
-y_out = torch.round(torch.sigmoid(y_out)).int()  
-count_pred = torch.bincount(y_out)
-print("count_pred:", count_pred)
-count_true = torch.bincount(y_test)
-print("count_true:", count_true)
-
-y_out = model(X_train).ravel()
-y_out = torch.round(torch.sigmoid(y_out)).int()  
-count_pred = torch.bincount(y_out)
-print("count_pred:", count_pred)
-count_true = torch.bincount(y_train)
-print("count_true:", count_true)
+#save the model
+state_dict = model.state_dict()
+current_path = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_path,NAME+".pth")
+if not os.path.exists(model_path):
+    torch.save(state_dict,model_path)
+print("Done!!! Your model is saved under name {}".format(NAME+".pth"))
