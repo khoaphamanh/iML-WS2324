30aa3a979519887ae62f31c4a5d91affbb7424f1
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-02-21 18:40:34 +0100

    add utils

--- utils.py
+++ utils.py
@@ -26,10 +26,13 @@ name_blackbox_nn = "neural_network"
 name_blackbox_rf = "random_forest"
 n_estimators_blackbox = 100
 
-#Parameter for generator as MCD_VAE
+#Parameter for generator as MCD_VAE in paper
 name_vae = "mcd_vae"
 origin_dim_vae = 9
 intermediate_dim_vae = 8
+num_gen = 100
+batch_size_vae = 100
+
 latent_dim_vae = origin_dim_vae // 2
 epoch_vae = 100
 dropout_vae = 0.3
@@ -37,5 +40,16 @@ batch_size_vae = 100
 lr_vae = 0.001
 wd_vae = 1e-4
 landa_vae = 1
-num_gen = 100
 name_gen = "gen_data"
+
+#Parameter for generator as MCD_VAE custom
+name_vae_custom = "mcd_ae_custom"
+intermediate_dim_vae_custom = 64
+latent_dim_vae_custom = 2
+epoch_vae_custom = 100
+dropout_vae_custom = 0.05
+lr_vae_custom = 0.01
+wd_vae_custom = 1e-5
+landa_vae_custom = 0.1
+name_gen_custom = "gen_data_custom"
+name_metrics_custom = "metrics_ae_custom"
