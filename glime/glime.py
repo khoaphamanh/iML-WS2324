04c84c05f801186bc28db9ba92a8bf0d28ce99f3
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-02-25 23:54:05 +0100

    add glime final

--- glime/glime.py
+++ glime/glime.py
@@ -7,172 +7,100 @@ sys.path.append(project_path)
 
 import utils
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import argparse
+#from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split
 import joblib
+import torch
 from generator.perturb import pertube_data_generator
 from generator.mcd_ae_custom import MonteCarloDropoutAutoEncoderCustom
 from generator.mcd_vae import MonteCarloDropoutVariationalAutoEncoder
+from adversarial.adversarial import AdversarialModel
 
-#constante
+#parameter lime
 SEED = utils.seed
 np.random.seed(SEED)
-NUM_PERTUBE = utils.num_pertube
+NUM_GEN_POINTS = utils.num_gen_points
+
+#constant datasets
+FEATURE_NAME = utils.feature_name
+UNRELATED_FEATURE_INDEX = utils.unrelated_feature_index
+CATEGORICAL_FEATURE_INDEX = utils.categorical_feature_index
+RACE_FEATURE_INDEX = utils.race_feature_index
+TEST_SIZE = utils.test_size
 NUMERICAL_FEATURE_INDEX = utils.numerical_feature_index
-
-NAME_MODEL = utils.name_blackbox_nn 
-INPUT_SIZE = utils.input_size 
-HIDDEN_SIZE = utils.hidden_size
-OUTPUT_SIZE = utils.output_size
-HIDDEN_LAYERS = utils.hidden_layers
-
-# add argument
-parse = argparse.ArgumentParser()
-parse.add_argument("--pertube_std", type=float, default=utils.pertube_std,
-                   help="name of the pretrained generator model name.pth, default is '{}'".format(utils.pertube_std))
-parse.add_argument("--num_pertube", type=int, default=utils.num_pertube,
-                   help="number of intermediate dimensions in the first hidden layer in model, default is {}".format(utils.num_pertube))
-
-# read the argument
-args = parse.parse_args()
-PERTUBE_STD = args.pertube_std
-NUM_PERTUBE = args.num_pertube
-
-#load data
-name_X = utils.name_preprocessed_data_X
-name_y = utils.name_preprocessed_data_y
-path_X = os.path.join(project_path,"data",name_X)
-path_y = os.path.join(project_path,"data",name_y)
-X = pd.read_csv(path_X,index_col=0)
-y = pd.read_csv(path_y,index_col=0).to_numpy().flatten()
-
-# add unrelated columns, setup
-# X['unrelated_column_one'] = np.random.choice([0,1],size=X.shape[0])
-# X['unrelated_column_two'] = np.random.choice([0,1],size=X.shape[0])
-X = X.to_numpy()
-
-# pertube data
-def pertube_data (X:np.array,x:np.array,numerical_feature_index: list,num_pertube:int,pertube_std:float):
-    """ 
-    This function will create neighbor points by adding Gaussian noise around a single instance. 
     
-    Parameters:
-	    X (np.array): datasets
-        x (np.array): a single instance
-        num_pertube (int): number of neighbor points
-        pertube_std (float): standivation of gausian noise
-    Returns:
-	    new_X (np.array): real data and pertube samples shape (n_samples, num_pertube + 1, n_feature). num_pertube + 1 contains 1 real sample and num_pertube sample from Gaussian nois
-    """
-    
-    #Normalize the data using standardize
-    scaler = StandardScaler()
-    scaler.fit(X)
-    x = scaler.transform(x.reshape(1, -1)).flatten()
-    
-    #a list contains and sampled x
-    new_X = []
-    
-    for n in range(num_pertube):
-        x_ = np.array([x[i] + np.random.normal(0,pertube_std,1)[0] if i in numerical_feature_index else x[i]  for i in range(len(x))])
-        new_X.append(x_)
-    
-    #inverse transform the sampled x
-    new_X = np.array(new_X)
-    new_X = scaler.inverse_transform(new_X)
-    
-    return new_X
+#parameter adversarial
+N_ESTIMATORS = utils.n_estimators_classifier
+TEST_SIZE = utils.test_size
+NAME_CLASSIFIER = utils.name_classifier
 
-# load interpretabel model
-model = LogisticRegression()
+#parameter generator VAE
+ORIGIN_DIM = utils.origin_dim_vae
+NAME_VAE = utils.name_vae
+NAME_GEN_VAE = utils.name_gen
+INTERMEDIATE_DIM_VAE = utils.intermediate_dim_vae
+LATENT_DIM_VAE = utils.latent_dim_vae
+DROPOUT_VAE = utils.dropout_vae
+NAME_SCALER = utils.name_scaler_vae
 
-# adversarial (black box) model
-import torch
-from torch import nn
+#parameter generator VAE
+NAME_VAE_CUSTOM = utils.name_vae_custom
+NAME_GEN_CUSTOM = utils.name_gen_custom
+INTERMEDIATE_DIM_CUSTOM = utils.intermediate_dim_vae_custom
+LATENT_DIM_CUSTOM = utils.latent_dim_vae_custom
+DROPOUT_CUSTOM = utils.dropout_vae_custom 
 
-#create model
-class BlackBoxModel(nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, hidden_layers:int, output_size:int ):
-        super().__init__()
-        self.input_layer = nn.Linear(input_size, hidden_size)
-        self.hidden_layers = nn.ModuleList([nn.Sequential(nn.Linear(hidden_size, hidden_size), nn.ReLU()) for i in range(hidden_layers)])
-        self.relu = nn.ReLU()
-        self.output_layer = nn.Linear(hidden_size, output_size)
-    
-    def forward(self, x):
-        x = self.input_layer(x)
-        x = self.relu(x)
-        for hid_layer in self.hidden_layers:
-            x = hid_layer(x)
-            x = self.relu(x)
-        x = self.output_layer(x)
-        return x
-    
-    def predict(self,x:np.array,prob = True):
-        x = torch.tensor(x).float()
-        with torch.inference_mode():
-            out =  self.forward(x)
-            if prob:
-                return torch.sigmoid(out).numpy().ravel().astype(int)
-            else:
-                return out.numpy().ravel()
+#parameter for perturb generator
+PERTURB_STD = utils.pertube_std
+NAME_GEN_PERTURB = utils.name_pertube_gen
 
-#pertube data
-scaler = StandardScaler()
+#parameter lime
+TEXT_RESULT_NAME = utils.text_result_name
 
-#load model
-state_dict_path = os.path.join(project_path,"blackbox", NAME_MODEL+ ".pth")
-state_dict = torch.load(state_dict_path)
-model = BlackBoxModel(input_size=INPUT_SIZE,hidden_size=HIDDEN_SIZE,output_size=OUTPUT_SIZE,hidden_layers=HIDDEN_LAYERS)
-model.load_state_dict(state_dict)
-
-#lime
 class LimeCustom:
-    def __init__(self, blackbox: nn.Module):
+    def __init__(self, blackbox: AdversarialModel, seed:int):
         """
         This class will have the misstion to calculate LIME from scratch. 
         
         Parameters:
-            black_box : blackbox model, which can used to predict the label, needs to have a .predict function to get a label of input x
+            blackbox (AdversarialModel): blackbox model, which can used to predict the label of an instance x
+            seed (int): random seed
         """
         self.black_box = blackbox
+        self.seed = seed
         
-    def get_lime (self, instance: np.array, sampled_instance: np.array, seed:int,sigma = None):
+    def get_lime (self, sampled_instance: np.array, name_gen:str,sigma = None):
         """
         In this method we will apply LIME to explane an instance using interpretable model
         
         Parameters:
             inter_model (LogisticRegression): Interpretable model. We only use Linear Regression model in sklearn
-            instance (np.array): instance needs to be explaned
-            sampled_instance (np.array): sampled instance around x
-            seed (int): random seed
+            sampled_instance (np.array): x and sampled instance from x, real x at index 0, fake x at the rest
+            name_gen (str): Name of the generator that used for sampling fake data
         """
+       
+        #add uncorrelated features
+        X = self.black_box.add_uncorrelated_feaatures(sampled_instance)
+        instance = X[0]
+        X, y = self.black_box.evaluate(name_gen,X)
+        if len(np.unique(y)) == 1:
+            return 1,np.zeros(shape=(1,X.shape[-1]))
         
         #calculate phi
-        X, phi = self.weights(instance=instance,sampled_instance=sampled_instance,sigma=sigma)
-        #print("X shape:", X.shape)
-        
+        X, phi = self.weights(sampled_instance=X,sigma=sigma)
+
         #train the interpretable mode
-        inter_model = LogisticRegression(random_state=seed)
-        scaler_path = os.path.join(project_path,"blackbox","mms_scaler.bin")
-        scaler = joblib.load(scaler_path)
-        
-        X = scaler.transform(X)
-        #print("X:", X[10:20])
-        y = self.black_box.predict(X)
-        print("y:", y)
+        inter_model = LogisticRegression(random_state=self.seed)
         inter_model.fit(X,y, sample_weight=phi)
 
         #contribution each feature to the result
         weights = inter_model.coef_
-        print("weights:", weights)
         bias = inter_model.intercept_
-        print("bias:", bias)
-        contribution = instance.reshape(1,-1) * weights
+        contribution = np.abs(instance.reshape(1,-1) * weights)
         
         return inter_model, contribution
 
-    def weights(self,instance:np.array, sampled_instance: np.array, sigma = None ):
+    def weights(self, sampled_instance: np.array, sigma = None):
         """
         In this method we will calculate the weights of instance x and sampled_instance.
         
@@ -190,26 +118,99 @@ class LimeCustom:
             sigma = np.sqrt(x.shape[-1]) * 0.75
             
         #calculate phi
-        sampled_instance = np.concatenate((x.reshape(1,-1),sampled_instance),axis=0)
-        d = np.sqrt(np.sum((instance - sampled_instance)**2,axis=1))
+        real_instance = sampled_instance[0]
+        d = np.sqrt(np.sum((real_instance - sampled_instance)**2,axis=1))
         phi = np.exp((-d**2) / (sigma**2))
 
         return sampled_instance, phi
+
+#run localy in this file
+if __name__ == "__main__":
+
+    #load data
+    name_X = utils.name_preprocessed_data_X
+    name_y = utils.name_preprocessed_data_y
+    path_X = os.path.join(project_path,"data",name_X)
+    path_y = os.path.join(project_path,"data",name_y)
+    X = pd.read_csv(path_X,index_col=0).to_numpy()
+    y = pd.read_csv(path_y,index_col=0).to_numpy().flatten()
+
+    #split data
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=TEST_SIZE,random_state=SEED)
+
+    #load adversarial model (same roll as blackbox)
+    adv = AdversarialModel(n_estimators=N_ESTIMATORS,name_classifier=NAME_CLASSIFIER,test_size=TEST_SIZE,seed=SEED,race_feature_index=RACE_FEATURE_INDEX,unrelated_feature_index=UNRELATED_FEATURE_INDEX)
+
+    #init lime
+    glime = LimeCustom(blackbox=adv,seed=SEED)
     
-test = LimeCustom(blackbox=model)
-inter_model = LogisticRegression()
-x = X[50]
-sampled_instance = pertube_data(X,x,NUMERICAL_FEATURE_INDEX,NUM_PERTUBE,PERTUBE_STD)
-#print("sampled_instance:", sampled_instance)
-#sampled_instance = np.concatenate((x.reshape(1,-1),pertube_data(X,x,NUMERICAL_FEATURE_INDEX,NUM_PERTUBE,PERTUBE_STD)),axis=0) 
-#print("sampled_instance:", sampled_instance)
-#print("sampled_instance:", sampled_instance)
-# lemon = test.get_lime(inter_model=inter_model,instance=x,sampled_instance=sampled_instance)
-_, w = test.weights(instance=x,sampled_instance=sampled_instance)
-#print("w shape:", w.shape)
-#print("w:", w)
+    #load generator
+    
+    #generator VAE
+    generator_VAE = MonteCarloDropoutVariationalAutoEncoder(origin_dim=ORIGIN_DIM,intermediate_dim=INTERMEDIATE_DIM_VAE,latent_dim=LATENT_DIM_VAE,dropout=DROPOUT_VAE)
+    model_path_VAE = os.path.join(project_path, "generator",NAME_VAE+".pth") 
+    state_dict_VAE = torch.load(model_path_VAE)
+    generator_VAE.load_state_dict(state_dict_VAE)
 
-model ,contribution = test.get_lime(instance=x,sampled_instance=sampled_instance,seed=SEED)
-print("contribution:", contribution)
+    #load scaler
+    scaler_path = os.path.join(project_path,"generator",NAME_SCALER+".bin")
+    scaler = joblib.load(scaler_path)
+    
+    #generator AE cusom
+    generator_AE = MonteCarloDropoutAutoEncoderCustom(origin_dim=ORIGIN_DIM,intermediate_dim=INTERMEDIATE_DIM_CUSTOM,latent_dim=LATENT_DIM_CUSTOM,dropout=DROPOUT_CUSTOM)
+    model_path_AE = os.path.join(project_path, "generator", NAME_VAE_CUSTOM+".pth") 
+    state_dict_AE = torch.load(model_path_AE)
+    generator_AE.load_state_dict(state_dict_AE)
 
+    #name pretraied classifier
+    name_gen = [NAME_GEN_VAE,NAME_GEN_CUSTOM,NAME_GEN_PERTURB]
+    generator = [generator_VAE,generator_VAE]
+    name_generator = [NAME_VAE,NAME_GEN_CUSTOM]
+    
+    #use glime for MCD_VAE and AE_CUSTOM
+    result = ""
+    for name in name_gen:
+        for idx ,gen in enumerate(generator):
+            contri = []
+            for x in X_test:
+                
+                #normalized x
+                x_scaled = scaler.transform(x.reshape(1,-1)).ravel()
+                
+                #sampled x
+                x_sampled = gen.generate_data(x = x_scaled,num_gen=NUM_GEN_POINTS,scaler=scaler,categorical_feature_index=CATEGORICAL_FEATURE_INDEX, return_label = False)
+
+                #use glime to calculate the contribution of significan feature value
+                _, contribution = glime.get_lime(sampled_instance=x_sampled,name_gen=name)
+                argmax_contribution = np.argmax(contribution,axis=1)
+                contri.append(argmax_contribution[0])
+            
+            contri = np.array(contri)
+            accuracy = np.sum(contri == RACE_FEATURE_INDEX) / len(contri)
+            text = "Result of adversarial trained on {} data evaluated on {} data: Accuracy {} \n".format(name,name_generator[idx],accuracy)
+            result = result + text
+
+    #use glime for perturb generator
+    for name in name_gen:
+        contri = []
+        for x in X_test:
+            #sampled x
+            x_sampled = pertube_data_generator(X = X,x=x,numerical_feature_index=NUMERICAL_FEATURE_INDEX,num_pertube=NUM_GEN_POINTS,pertube_std=PERTURB_STD, return_label = False)
+            
+            #use glime to calculate the contribution of significan feature value
+            _, contribution = glime.get_lime(sampled_instance=x_sampled,name_gen=name)
+            argmax_contribution = np.argmax(contribution,axis=1)
+            contri.append(argmax_contribution[0])
+
+        contri = np.array(contri)
+        accuracy = np.sum(contri == RACE_FEATURE_INDEX) / len(contri)
+        text = "Result of adversarial trained on {} data evaluated on {} data: Accuracy {} \n".format(name,name_generator[idx],accuracy)
+        result = result + text
+        
+#save the result as text
+current_path = os.path.dirname(os.path.abspath(__file__))
+result_path = os.path.join(current_path,TEXT_RESULT_NAME+".txt")
+if not os.path.exists(result_path):
+    with open(result_path,"w") as file:
+        file.write(result)
         
